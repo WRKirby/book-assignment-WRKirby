0ccaa8e95bf4d8c4672b32d06e8b014f8b827563
root <root@localhost>
2021-10-01 03:01:43 +0000

    final push hopefully

--- app/views/books/_book.json.jbuilder
+++ app/views/books/_book.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! book, :id, :title, :created_at, :updated_at
+json.extract! book, :id, :title, :price, :date, :author, :created_at, :updated_at
 json.url book_url(book, format: :json)
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,26 +2,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid title' do
+  scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    fill_in 'Price', with: '20.00'
     fill_in 'Author', with: 'JK Rowling'
-    fill_in 'Date', with: '02/02/02'
+    fill_in 'Price', with: '20.01'
+    fill_in 'Date', with: '2000-03-10'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
-  end
-
-  scenario 'valid price' do
-    expect(page).to have_content('20.00')
-  end
-
-  scenario 'valid author' do
     expect(page).to have_content('JK Rowling')
-  end
+    expect(page).to have_content('2000-03-10')
+    expect(page).to have_content('20.01')
 
-  scenario 'valid date' do
-    expect(page).to have_content('02/02/02')
   end
 end
